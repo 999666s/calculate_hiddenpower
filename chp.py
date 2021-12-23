6deeeceb2f9f2c4a58f30cdc1b0dd53d4e1ea638
999666s <ninesix666999@mail.ru>
2021-12-23 14:56:14 +0300

    Move types to calculate_hiddenpower func

--- chp.py
+++ chp.py
@@ -4,24 +4,7 @@ from re import findall
 from sys import argv
 
 
-types = {
-    0: "Bug",
-    1: "Dark",
-    2: "Dragon",
-    3: "Electric",
-    4: "Fight",
-    5: "Fire",
-    6: "Flight",
-    7: "Ghost",
-    8: "Grass",
-    9: "Earth",
-    10: "Ice",
-    11: "Water",
-    12: "Poison",
-    13: "Psycho",
-    14: "Stone",
-    15: "Steel"
-}
+
 
 
 def hp_from_stats(a, b, c, d, e, f):
@@ -33,6 +16,24 @@ def get_stats_from_gen(gen: str) -> list[str]:
 
 
 async def calculate_hiddenpower(gen: str):
+    types = {
+        0: "Bug",
+        1: "Dark",
+        2: "Dragon",
+        3: "Electric",
+        4: "Fight",
+        5: "Fire",
+        6: "Flight",
+        7: "Ghost",
+        8: "Grass",
+        9: "Earth",
+        10: "Ice",
+        11: "Water",
+        12: "Poison",
+        13: "Psycho",
+        14: "Stone",
+        15: "Steel"
+    }
     try:
         stats = map(lambda stat: int(stat % 2), map(int, get_stats_from_gen(gen)))
         hp = types[hp_from_stats(*stats)]
